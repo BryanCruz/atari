c8802762d1288ad103b1b713c38c39def13c0944
jonatas57 <jdjonatasduarte57@gmail.com>
2018-06-19 20:10:03 +0000

    aparentemente funcionando

--- tictactoe/Game.java
+++ tictactoe/Game.java
@@ -1,64 +1,28 @@
-package tictactoe;
+package game;
 
 import java.util.Scanner;
-import tsk.Board;
-public class Game{
-	public static void main(String[] args){
+import backend.Board;
+
+public class Game {
+	public static void main(String[] args) {
 		Board board = new Board();
 
 		Scanner scanner = new Scanner(System.in);
 
-		int countCells = 0;
-		while(countCells < 9 && !checkWin(board)){
+		while(!board.getGameOver()) {
 			int i = scanner.nextInt();
 			int j = scanner.nextInt();
 
-			if(i >= 0 && i < 3 && j >= 0 && j < 3 && board.getCells()[i][j] == '-'){
+			if (i >= 0 && i < 3 && j >= 0 && j < 3 && board.getCells()[i][j] == '-') {
 				board.play(i, j);
-				countCells++;
 			}
 		}
 
-		if(checkWin(board)){
-			System.out.println("You win!!");
-		}else{
+		if (board.checkWin()) {
+			System.out.printf("Player %d win!!\n", board.getWinner());
+		}
+		else {
 			System.out.println("Tie!!");
-		}
-	}
-
-	public static boolean checkWin(Board board){
-		char[][] cells = board.getCells();
-
-		int checkLine      = 0;
-		int checkColumn    = 0;
-		int checkDiagonal1 = 0;
-		int checkDiagonal2 = 0;
-
-		char charDiagonal1 = cells[0][0];
-		char charDiagonal2 = cells[0][2];
-
-		if(charDiagonal1 == '-') charDiagonal1 = ' ';
-		if(charDiagonal2 == '-') charDiagonal2 = ' ';
-
-		for(int i = 0; i < 3 && checkLine < 3 && checkColumn < 3; i++){
-			char charColumn = cells[0][i];
-			char charLine   = cells[i][0];
-
-			if(charColumn == '-') charColumn = ' ';
-			if(charLine   == '-') charLine   = ' ';
-
-			if(cells[i][i]   == charDiagonal1)   checkDiagonal1++;
-			if(cells[i][2-i] == charDiagonal2)   checkDiagonal2++;
-
-			for(int j = 0; j < 3; j++){
-				if(cells[i][j] == charLine)   checkLine++;
-				if(cells[j][i] == charColumn) checkColumn++;
-			}
-		}
-		if(checkLine == 3 || checkColumn == 3 || checkDiagonal1 == 3 || checkDiagonal2 == 3){
-			return true;
-		}else{
-			return false;
 		}
 	}
 }
--- tictactoe/backend/Board.java
+++ tictactoe/backend/Board.java
@@ -1,28 +1,90 @@
-package tictactoe.backend;
+package backend;
 
-public class Board{
+public class Board {
 	private char[][] cells;
-	private boolean player1;
+	private int[] checkLine, checkColumn, checkDiagonal;
+	private boolean player1, gameOver;
+	private int countCells, winner;
 
-	public Board(){
+	public Board() {
 		this.cells = new char[3][3];
+		this.checkLine = new int[3];
+		this.checkColumn = new int[3];
+		this.checkDiagonal = new int[3];
 		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
+			for(int j = 0; j < 3; j++) {
 				this.cells[i][j] = '-';
 			}
 		}
-
 		this.player1 = true;
 	}
 
 	public char[][] getCells(){
 		return this.cells;
 	}
+	public void setCells(int i, int j) {
+		char form = getPlayer1() ? 'X' : 'O';
+		this.cells[i][j] = form;
+	}
+
+	public boolean getPlayer1() {
+		return this.player1;
+	}
+	public void setPlayer1(boolean value) {
+		this.player1 = value;
+	}
+
+	public int getCountCells() {
+		return countCells;
+	}
+	public void setCountCells(int count) {
+		this.countCells = count;
+	}
+
+	public boolean getGameOver() {
+		return gameOver;
+	}
+	public void setGameOver(boolean value) {
+		this.gameOver = value;
+	}
+
+	public int getWinner() {
+		return this.winner;
+	}
+	public void setWinner(int w) {
+		this.winner = w;
+	}
 
 	public void play(int i, int j){
-		char form = player1 ? 'X' : 'O';
-		this.cells[i][j] = form;
+		//preenche célula
+		setCells(i, j);
+		//conta células preenchidas
+		setCountCells(getCountCells() + 1);
+		if (getCountCells() == 9 || checkCell(i, j) != 0) setGameOver(true);
 
-		this.player1 = !this.player1;
+		//troca jogador
+		setPlayer1(!getPlayer1());
+	}
+
+	public int checkCell(int i, int j){
+		this.checkLine[i] += player1 ? 1 : -1;
+		this.checkColumn[j] += player1 ? 1 : -1;
+		if (i + j % 2 == 0) {
+			this.checkDiagonal[i == j ? 0 : 1] += player1 ? 1 : -1;
+		}
+
+		if (this.checkLine[i] == 3 || this.checkColumn[j] == 3 || this.checkDiagonal[0] == 3 || this.checkDiagonal[1] == 3) {
+			setWinner(1);
+			return 1;
+		}
+		else if (this.checkLine[i] == -3 || this.checkColumn[j] == -3 || this.checkDiagonal[0] == -3 || this.checkDiagonal[1] == -3) {
+			setWinner(2);
+			return -1;
+		}
+		else return 0;
+	}
+	public boolean checkWin() {
+		if (getWinner() != 0) return true;
+		return false;
 	}
 }
