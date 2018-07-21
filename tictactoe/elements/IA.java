f341953a68284a3f50916de4c60fc4506e77cad7
jonatas57 <jdjonatasduarte57@gmail.com>
2018-07-21 20:52:29 +0000

    metodo makeChoice adicionado

--- tictactoe/elements/IA.java
+++ tictactoe/elements/IA.java
@@ -1,9 +1,31 @@
 package tictactoe.elements;
 
+import java.util.Random;
+
 public class IA implements Player {
-	
+
+	private Board board;
+	private int difficulty;
+	private Random random;
+
+	public IA(int difficulty, Board board) {
+		this.difficulty = difficulty;
+		this.board = board;
+		random = new Random();
+	}
+
 	@Override
 	public void play() {
-		
+		int[] choice = makeChoice(board);
+		board.setCell(choice[0], choice[1], )
+	}
+
+	public int[] makeChoice(Board board) {
+		int x, y;
+		do {
+			x = Math.abs(random.nextInt()) % board.getBoardSize();
+			y = Math.abs(random.nextInt()) % board.getBoardSize();
+		} while (!Engine.checkEmptyCell(board, x, y));
+		return (new int[] {x, y});
 	}
 }
