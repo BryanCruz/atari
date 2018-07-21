9d9970d172f3e3bb6ddd3db4d46007bb533a5913
filipibrabo <filipi.brabo@gmail.com>
2018-07-21 18:22:49 -0300

    Adicionando codigo da classe Main

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import java.util.Scanner;
+
+class Main {
 	public static void main(String[] args) {
-		tictactoe.Game.startGame();
+		private boolean multiplayer;
+		private int boardSize, difficultyIA;
+		
+		boardSize = 3;
+		
+		Interface.firstScreen;		
+		sc.nextInt() = 1 ? multiplayer = false : multiplayer = true;
+		
+		if (!multiplayer) {
+			Interface.difficultyScreen();
+			difficultyIA = sc.nextInt();
+		}	
+		
+		tictactoe.Game.startGame(boardSize, multiplayer, difficultyIA);
 	}
 }
