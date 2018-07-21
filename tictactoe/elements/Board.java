aa2b3ac93fc97c3c56b2513a6d013110098b8e96
jonatas57 <jdjonatasduarte57@gmail.com>
2018-07-21 21:36:18 +0000

    getCell fixed

--- tictactoe/elements/Board.java
+++ tictactoe/elements/Board.java
@@ -1,4 +1,4 @@
-package tictactoe.elements;
+package tictactoe.backend;
 
 public class Board {
 	private char[][] cells;
@@ -6,22 +6,22 @@ public class Board {
 	public Board(int boardSize) {
 		this.setCells(new char[boardSize][boardSize]);
 	}
-	
+
 	private char[][] getCells() {
 		return this.cells;
 	}
-	private void setCells(char[][] cells){
+	private void setCells(char[][] cells) {
 		this.cells = cells;
 	}
 
-	public void getCell(i, j){
+	public void getCell(int i, int j) {
 		return this.getCells()[i][j];
 	}
 	public void setCell(int i, int j, char c) {
 		this.getCells()[i][j] = c;
 	}
 
-	public int getBoardSize(){
+	public int getBoardSize() {
 		return this.getCells().length;
 	}
 }
