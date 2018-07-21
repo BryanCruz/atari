5e11ff214d9d187fd84ca675b811068067b46d8d
jonatas57 <jdjonatasduarte57@gmail.com>
2018-07-21 22:26:03 +0000

    else sem if fixed

--- tictactoe/Game.java
+++ tictactoe/Game.java
@@ -7,19 +7,19 @@ import tictactoe.frontend.Interface;
 
 public class Game {
 	public static void startGame(int boardSize, int difficulty) {
-		
+
 		Board board = new Board(boardSize);
 		Engine.clearBoard(board);
 		Scanner scanner = new Scanner(System.in);
-		
-		Interface.nameScreen(1);		
+
+		Interface.nameScreen(1);
 		Player player1 = new Human(scanner.nextLine(), 1);
-		
+
 		if (difficulty < 0) {
 			//If it's multiplayer, instatiates a new Human
-			Interface.nameScreen(2);			
-			Player player2 = new Human(scanner.nextLine(), 2); 		
-		} else {			
+			Interface.nameScreen(2);
+			Player player2 = new Human(scanner.nextLine(), 2);
+		} else {
 			//If it's singleplayer, instatiates an IA
 			Player player2 = new IA(difficulty, 2);
 		}
@@ -34,15 +34,16 @@ public class Game {
 				Interface.printBoard(board);
 			}
 		}
-		
-		
+
+
 		if (Engine.checkWin(board) == 0) {
 			Interface.tieScreen();
 		} else if (Engine.checkWin(board) == 1){
 			if (multiplayer) {
 				Interface.winnerScreen(player1.getName());
 			}else {
-				Inteface.winnerScreen("YOU");
+				Interface.winnerScreen("YOU");
+			}
 		}else {
 			if (multiplayer) {
 				Interface.winnerScreen(player2.getName());
