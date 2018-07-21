3737b88d0310fefca1a0b5c6d761b8727ecb9a1d
filipibrabo <filipi.brabo@gmail.com>
2018-07-21 16:44:24 -0300

    Classe Human.java adicionada

---
+++ tictactoe/elements/Human.java
@@ -0,0 +1,18 @@
+package tictactoe.elements;
+
+public class Human implements Player {
+	private String name;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName() {
+		this.name = name;
+	}
+	
+	@Override
+	public void play() {
+		
+	}
+}
