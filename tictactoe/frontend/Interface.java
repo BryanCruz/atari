a35532bd4f5b2cd21c92358208f8d8a62f8306a9
BryanCruz <bryan.bialokur@gmail.com>
2018-07-21 18:52:33 -0300

    metodo trocado

--- tictactoe/frontend/Interface.java
+++ tictactoe/frontend/Interface.java
@@ -9,7 +9,7 @@ public abstract class Interface {
 	public static void printBoard(Board board) {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				System.out.printf(" %c %c", board.getCells()[i][j], (j == 2 ? '\n' : '|'));
+				System.out.printf(" %c %c", board.getCell([i][j]), (j == 2 ? '\n' : '|'));
 			}
 		}
 	}
