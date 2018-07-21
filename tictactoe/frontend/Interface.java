64334a211c8c2db3d6cc7200007568d9b2f8f106
BryanCruz <bryan.bialokur@gmail.com>
2018-07-21 14:14:09 -0300

    muda para um método estático

--- tictactoe/frontend/Interface.java
+++ tictactoe/frontend/Interface.java
@@ -3,9 +3,9 @@ package tictactoe.frontend;
 import tictactoe.backend.Board;
  
 public class Interface {
-	public void printBoard(Board board) {
-		for (int i = 0;i < 3;i++) {
-			for (int j = 0;j < 3;j++) {
+	public static void printBoard(Board board) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
 				System.out.printf("%c%c", board.getCells()[i][j], (j == 2 ? '\n' : '|'));
 			}
 		}
