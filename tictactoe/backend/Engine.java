a8c110ee0b4c6de64ba658c4a5f1f0b9eeb0eb29
jonatas57 <jdjonatasduarte57@gmail.com>
2018-07-21 20:23:44 +0000

    checkWin refeito

--- tictactoe/backend/Engine.java
+++ tictactoe/backend/Engine.java
@@ -2,20 +2,21 @@ package tictactoe.backend;
 
 public abstract class Engine {
 
+  private static int size;
   private static int countCells = 0;
   private static final char emptySymbol = '-';
 
-  private static void clearBoard(Board board){
-    int n = board.getBoardSize();
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < n; j++){
+  private static void clearBoard(Board board) {
+    int size = board.getBoardSize();
+    for(int i = 0; i < size; i++){
+      for(int j = 0; j < size; j++){
         board.setCell(i, j, emptySymbol);
       }
     }
   }
 
   public static void play() {
-    
+
   }
 
   public static boolean checkEmptyCell(Board board, int i, int j){
@@ -23,19 +24,43 @@ public abstract class Engine {
   }
 
   public static int checkWin(Board board) {
-    for (int i = 0;i < 3;i++) {
-      if (board.checkColumn[i] == 3 || board.checkLine[i] == 3 || board.checkDiagonal[i] == 3) {
-        return 1;
+    for (int i = 0;i < size;i++) {
+      int checkLine = 1;
+      for (int j = 0;j < size;j++) {
+        char c = board.getCell(i, j);
+        checkLine *= (c == 'x' ? 2 : (c == 'o' ? 3 : 0));
       }
-      if (board.checkColumn[i] == -3 || board.checkLine[i] == -3 || board.checkDiagonal[i] == -3) {
-        return 2;
-      }
-      return 0;
+      if (c == 8) return 1;
+      if (c == 27) return 2;
     }
+    for (int i = 0;i < size;i++) {
+      int checkColumn = 1;
+      for (int j = 0;j < size;j++) {
+        char c = board.getCell(j, i);
+        checkColumn *= (c == 'x' ? 2 : (c == 'o' ? 3 : 0));
+      }
+      if (c == 8) return 1;
+      if (c == 27) return 2;
+    }
+    for (int i = 0;i < size;i++) {
+      int checkDiag1 = 1, checkDiag2 = 1;
+      for (int j = 0;j < size;j++) {
+        char c = board.getCell(j, i);
+        if (i == j) {
+          checkDiag1 *= (c == 'x' ? 2 : (c == 'o' ? 3 : 0));
+        }
+        if (i == size - j - 1) {
+          checkDiag2 *= (c == 'x' ? 2 : (c == 'o' ? 3 : 0));
+        }
+      }
+      if (checkDiag1 == 8 || checkDiag2 == 8) return 1;
+      if (checkDiag1 == 27 || checkDiag2 == 27) return 2;
+    }
+    return 0;
   }
 
   public static boolean gameOver(Board board) {
-    return (checkWin(board) != 0 || countCells == 9);
+    return (checkWin(board) != 0 || countCells == size * size);
   }
 
   public static void setCountCells() {
