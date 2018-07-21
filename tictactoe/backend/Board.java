1d83a98a8f04456c9313c2a9b99d02ea55fd1fee
BryanCruz <bryan.bialokur@gmail.com>
2018-07-21 14:46:27 -0300

    modulariza a Board para ter apenas métodos relacionados à uma board de jogo da velha

--- tictactoe/backend/Board.java
+++ tictactoe/backend/Board.java
@@ -2,98 +2,40 @@ package tictactoe.backend;
 
 public class Board {
 	private char[][] cells;
-	private int[] checkLine, checkColumn, checkDiagonal;
-	private boolean player1, gameOver;
-	private int countCells, winner;
 
-	public Board() {
-		this.cells = new char[3][3];
-		this.checkLine = new int[3];
-		this.checkColumn = new int[3];
-		this.checkDiagonal = new int[3];
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++) {
-				this.cells[i][j] = '-';
-			}
-		}
-		this.player1 = true;
+	public Board(int boardSize) {
+		this.setCells(new char[boardSize][boardSize]);
+		this.clearBoard();
 	}
-
-	public void printBoard(){
-		char[][] cells = this.getCells();
-		for(int i = 0; i < cells.length; i++){
-			for(int j = 0; j < cells[0].length; j++){
-				System.out.printf("| %c |", cells[i][j]);
-			}
-			System.out.println();
-		}
-	}
-
+	
 	public char[][] getCells(){
 		return this.cells;
 	}
-	public void setCells(int i, int j) {
-		char form = getPlayer1() ? 'X' : 'O';
-		this.cells[i][j] = form;
+	private void setCells(char[][] cells){
+		this.cells = cells;
 	}
 
-	public boolean getPlayer1() {
-		return this.player1;
-	}
-	public void setPlayer1(boolean value) {
-		this.player1 = value;
-	}
-
-	public int getCountCells() {
-		return countCells;
-	}
-	public void setCountCells(int count) {
-		this.countCells = count;
+	public void setCell(int i, int j, char c) throws invalidSymbolException {
+		if(c == 'X' || c == 'O') 
+			this.getCells()[i][j] = c;
+		else
+			throw new InvalidSymbolException();
 	}
 
-	public boolean getGameOver() {
-		return gameOver;
-	}
-	public void setGameOver(boolean value) {
-		this.gameOver = value;
+	private void clearCell(int i, int j){
+		this.getCells()[i][j] = '-';
 	}
 
-	public int getWinner() {
-		return this.winner;
-	}
-	public void setWinner(int w) {
-		this.winner = w;
+	public int getBoardSize(){
+		return this.getCells().length;
 	}
 
-	public void play(int i, int j){
-		//preenche célula
-		setCells(i, j);
-		//conta células preenchidas
-		setCountCells(getCountCells() + 1);
-		if (getCountCells() == 9 || checkCell(i, j) != 0) setGameOver(true);
-
-		//troca jogador
-		setPlayer1(!getPlayer1());
-	}
-	public int checkCell(int i, int j){
-		this.checkLine[i] += player1 ? 1 : -1;
-		this.checkColumn[j] += player1 ? 1 : -1;
-		if (i + j % 2 == 0) {
-			this.checkDiagonal[i == j ? 0 : 1] += player1 ? 1 : -1;
+	public void clearBoard(){
+		char[][] cells = this.getCells();
+		for(int i = 0; i < cells.length; i++){
+			for(int j = 0; j < cells[0].length; j++){
+				this.clearCell(i, j);
+			}
 		}
-
-		if (this.checkLine[i] == 3 || this.checkColumn[j] == 3 || this.checkDiagonal[0] == 3 || this.checkDiagonal[1] == 3) {
-			setWinner(1);
-			return 1;
-		}
-		else if (this.checkLine[i] == -3 || this.checkColumn[j] == -3 || this.checkDiagonal[0] == -3 || this.checkDiagonal[1] == -3) {
-			setWinner(2);
-			return -1;
-		}
-		else return 0;
-	}
-	public boolean checkWin() {
-		if (getWinner() != 0) return true;
-		return false;
 	}
 }
