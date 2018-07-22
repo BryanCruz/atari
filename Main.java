3339f8247d1cdec9132ea4fa20f1c38954ddd60b
BryanCruz <bryan.bialokur@gmail.com>
2018-07-21 21:15:25 -0300

    add game loop

--- Main.java
+++ Main.java
@@ -4,25 +4,37 @@ import tictactoe.Game;
 
 class Main {
 	public static void main(String[] args) {
-		int boardSize, difficulty;
+
+		int restartOption = 2;
+		int boardSize = 3, difficulty = 0;
 		Scanner sc = new Scanner(System.in);
 
-		boardSize = 3;
+		while(restartOption != 3){
+			if(restartOption == 2){	
+				boardSize = 3;
 
-		Interface.firstScreen();
+				Interface.firstScreen();
 
-		// checks if it is a multiplayer game
-		boolean multiplayer = sc.nextInt() == 1;
-		if (multiplayer) {
-			difficulty = 0;
+				// checks if it is a multiplayer game
+				boolean multiplayer = sc.nextInt() == 1;
+				sc.nextLine();
+
+				if (multiplayer) {
+					difficulty = 0;
+				}
+				else {
+					// if it isn't, the player chooses a difficulty to play
+					Interface.difficultyScreen();
+					difficulty = sc.nextInt();
+					sc.nextLine();
+				}
+			}
+
+			// start a game
+			Game.startGame(boardSize, difficulty);
+			Interface.endScreen();
+			restartOption = sc.nextInt();
+			sc.nextLine();
 		}
-		else {
-			// if it isn't, the player chooses a difficulty to play
-			Interface.difficultyScreen();
-			difficulty = sc.nextInt();
-		}
-
-		// start a game
-		Game.startGame(boardSize, difficulty);
 	}
 }
